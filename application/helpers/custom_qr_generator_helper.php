38a70baa6ce9ed061f1ff94af2312ab65d5564b5
Fahmy Izwan <mfahmyizwan@gmail.com>
2023-02-27 23:05:22 +0800

    Update custom_qr_generator_helper.php

--- application/helpers/custom_qr_generator_helper.php
+++ application/helpers/custom_qr_generator_helper.php
@@ -19,47 +19,43 @@ if (!function_exists('generateQR')) {
 	{
 		$writer = new PngWriter();
 
-		if (empty($writer)) {
-			// Create QR code
-			$qrCode = QrCode::create($dataQR)
-				->setEncoding(new Encoding('UTF-8'))
-				->setErrorCorrectionLevel(new ErrorCorrectionLevelLow())
-				->setSize(300)
-				->setMargin(10)
-				->setRoundBlockSizeMode(new RoundBlockSizeModeMargin())
-				->setForegroundColor(new Color(0, 0, 0))
-				->setBackgroundColor(new Color(255, 255, 255));
+		// Create QR code
+		$qrCode = QrCode::create($dataQR)
+			->setEncoding(new Encoding('UTF-8'))
+			->setErrorCorrectionLevel(new ErrorCorrectionLevelLow())
+			->setSize(300)
+			->setMargin(10)
+			->setRoundBlockSizeMode(new RoundBlockSizeModeMargin())
+			->setForegroundColor(new Color(0, 0, 0))
+			->setBackgroundColor(new Color(255, 255, 255));
 
-			if (!empty($logo)) {
-				// Create generic logo
-				$fileLogo = $logo['image'];
-				$sizeLogo = $logo['size'];
+		if (!empty($logo)) {
+			// Create generic logo
+			$fileLogo = $logo['image'];
+			$sizeLogo = $logo['size'];
 
-				$logo = Logo::create($fileLogo)->setResizeToWidth($sizeLogo);
-			}
-
-			// Create generic label
-			if (!empty($labelQR)) {
-				$label = Label::create($labelQR)
-					->setTextColor(new Color(255, 0, 0));
-
-				$result = $writer->write($qrCode, $logo, $label);
-			} else {
-				$result = $writer->write($qrCode, $logo);
-			}
-
-			header('Content-Type: ' . $result->getMimeType());
-
-			// Save it to a file
-			$result->saveToFile($folder . '/' . $fileName);
-
-			return [
-				'qrFolder' => $folder,
-				'qrFilename' => $fileName,
-				'qrPath' => $folder . '/' . $fileName,
-			];
-		} else {
-			return [];
+			$logo = Logo::create($fileLogo)->setResizeToWidth($sizeLogo);
 		}
+
+		// Create generic label
+		if (!empty($labelQR)) {
+			$label = Label::create($labelQR)
+				->setTextColor(new Color(255, 0, 0));
+
+			$result = $writer->write($qrCode, $logo, $label);
+		} else {
+			$result = $writer->write($qrCode, $logo);
+		}
+
+		header('Content-Type: ' . $result->getMimeType());
+
+		// Save it to a file
+		$result->saveToFile($folder . '/' . $fileName);
+
+		return [
+			'qrFolder' => $folder,
+			'qrFilename' => $fileName,
+			'qrPath' => $folder . '/' . $fileName,
+		];
 	}
 }
