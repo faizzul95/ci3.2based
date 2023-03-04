ab4509f241eae52adabb25285542e8f0337cec32
Fahmy Izwan <mfahmyizwan@gmail.com>
2023-03-05 01:34:09 +0800

    Update Sanctum.php

--- application/middleware/Sanctum.php
+++ application/middleware/Sanctum.php
@@ -7,8 +7,12 @@ class Sanctum implements Luthier\MiddlewareInterface
 	public function run($args)
 	{
 		if (!checkMaintenance()) {
-			if (!isLoginCheck())
-				return response(['code' => 401, 'message' => 'Login is required!'], HTTP_UNAUTHORIZED);
+			if (!isLoginCheck()) {
+				if (isAjax())
+					return response(['code' => 401, 'message' => 'Login is required!'], HTTP_UNAUTHORIZED);
+				else
+					redirect('', true);
+			}
 		} else {
 			return response(['code' => 500, 'message' => 'System under maintenance'], HTTP_INTERNAL_ERROR);
 		}
