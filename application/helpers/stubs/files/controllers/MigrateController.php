5731d60c87b393639a7934ec0f3f3e80a188b288
Fahmy Izwan <mfahmyizwan@gmail.com>
2023-04-08 13:43:49 +0800

    change migrate files

--- application/helpers/stubs/files/controllers/MigrateController.php
+++ application/helpers/stubs/files/controllers/MigrateController.php
@@ -29,62 +29,64 @@ class MigrateController extends CI_Controller
 		if (filter_var(env('MIGRATION'), FILTER_VALIDATE_BOOLEAN)) {
 
 			$files = array_diff(scandir(APPPATH . 'migrations'), array('.', '..'));
-			echo '
-        <base href="' . baseURL() . '">
-        <meta name="base_url" content="' . baseURL() . '" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <script src="https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.js"></script>
-        <link href="' . asset('custom/css/toastr.min.css') . '" rel="stylesheet" type="text/css" />
 
-        <script src="' . asset('custom/js/jquery.min.js') . '"></script>
-        <script src="' . asset('custom/js/axios.min.js') . '"></script>
-        <script src="' . asset('custom/js/helper.js') . '"></script>
-        <script src="' . asset('custom/js/toastr.min.js') . '"></script>';
+			if (count($files) > 0) {
+				echo '
+					<base href="' . baseURL() . '">
+					<meta name="base_url" content="' . baseURL() . '" />
+					<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+					<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+					<script src="https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.js"></script>
+					<link href="' . asset('custom/css/toastr.min.css') . '" rel="stylesheet" type="text/css" />
 
-			echo '<div class="container mt-5">
-                <div class="row">
-                    <table class="table table-bordered table-striped table-responsive-sm" width="100%">
-                        <thead class="table-dark">
-                            <tr>
-                                <th> Filename </th>
-                                <th width="10%"> Status </th>
-                                <th width="25%"> Action </th>
-                            </td>
-                        </thead>
-                        <tbody>';
+					<script src="' . asset('custom/js/jquery.min.js') . '"></script>
+					<script src="' . asset('custom/js/axios.min.js') . '"></script>
+					<script src="' . asset('custom/js/helper.js') . '"></script>
+					<script src="' . asset('custom/js/toastr.min.js') . '"></script>';
 
-			foreach ($files as $fileName) {
+				echo '<div class="container mt-5">
+						<div class="row">
+							<table class="table table-bordered table-striped table-responsive-sm" width="100%">
+								<thead class="table-dark">
+									<tr>
+										<th> Filename </th>
+										<th width="10%"> Status </th>
+										<th width="25%"> Action </th>
+									</td>
+								</thead>
+								<tbody>';
 
-				$file = APPPATH . 'migrations/' . $fileName;
+				foreach ($files as $fileName) {
 
-				include_once($file);
-				$class = 'Migration_' . ucfirst(strtolower($this->_get_migration_name(basename($file, '.php'))));
-				$obj = new $class;
-				$tableName = $obj->table_name;
+					$file = APPPATH . 'migrations/' . $fileName;
 
-				$status = isTableExist($tableName) ? 1 : 0;
-				$statusBadge = [
-					0 => '<span class="badge bg-danger"> Not Exist </span>',
-					1 => '<span class="badge bg-success"> Exist </span>',
-				];
+					include_once($file);
+					$class = 'Migration_' . ucfirst(strtolower($this->_get_migration_name(basename($file, '.php'))));
+					$obj = new $class;
+					$tableName = $obj->table_name;
 
-				$actionUp =  $status == 1 ? '' : '<a class="btn btn-sm btn-outline-success me-2" href="javascript:void(0);" onclick="migration(\'up\', \'' . $fileName . '\')"> <span class="fa fa-plus"></span> Migrate </a>';
-				$actionDown =  $status != 1 ? '' : '<a class="btn btn-sm btn-outline-danger me-2" href="javascript:void(0);" onclick="migration(\'down\', \'' . $fileName . '\')"> <span class="fa fa-minus"></span> Drop </a>';
-				$actionSeed =  $status != 1 ? '' : '<a class="btn btn-sm btn-outline-primary me-2" href="javascript:void(0);" onclick="migration(\'seeder\', \'' . $fileName . '\')"> <span class="fa fa-database"></span> Seed </a>';
-				$actionTruncate =  $status != 1 ? '' : '<a class="btn btn-sm btn-outline-dark me-2" href="javascript:void(0);" onclick="migration(\'truncate\', \'' . $fileName . '\')"> <span class="fa fa-trash-alt"></span> Truncate </a>';
+					$status = isTableExist($tableName) ? 1 : 0;
+					$statusBadge = [
+						0 => '<span class="badge bg-danger"> Not Exist </span>',
+						1 => '<span class="badge bg-success"> Exist </span>',
+					];
 
-				echo '<tr>
-                    <td> &nbsp; ' . basename($fileName, '.php') . ' </td>
-                    <td> &nbsp; ' . $statusBadge[$status] . ' </td>
-                    <td>
-                        <center>' . $actionUp . $actionDown . $actionSeed . $actionTruncate . '
-                        </center>
-                    </td>
-                </tr>';
-			}
+					$actionUp =  $status == 1 ? '' : '<a class="btn btn-sm btn-outline-success me-2" href="javascript:void(0);" onclick="migration(\'up\', \'' . $fileName . '\')"> <span class="fa fa-plus"></span> Migrate </a>';
+					$actionDown =  $status != 1 ? '' : '<a class="btn btn-sm btn-outline-danger me-2" href="javascript:void(0);" onclick="migration(\'down\', \'' . $fileName . '\')"> <span class="fa fa-minus"></span> Drop </a>';
+					$actionSeed =  $status != 1 ? '' : '<a class="btn btn-sm btn-outline-primary me-2" href="javascript:void(0);" onclick="migration(\'seeder\', \'' . $fileName . '\')"> <span class="fa fa-database"></span> Seed </a>';
+					$actionTruncate =  $status != 1 ? '' : '<a class="btn btn-sm btn-outline-dark me-2" href="javascript:void(0);" onclick="migration(\'truncate\', \'' . $fileName . '\')"> <span class="fa fa-trash-alt"></span> Truncate </a>';
 
-			echo '          </tbody>
+					echo '<tr>
+										<td> &nbsp; ' . basename($fileName, '.php') . ' </td>
+										<td> &nbsp; ' . $statusBadge[$status] . ' </td>
+										<td>
+											<center>' . $actionUp . $actionDown . $actionSeed . $actionTruncate . '
+											</center>
+										</td>
+									</tr>';
+				}
+
+				echo '          </tbody>
                     </table>
                 </div>
             </div>
@@ -110,6 +112,9 @@ class MigrateController extends CI_Controller
             </script>
 
             ';
+			} else {
+				exit('No migrations files found!');
+			}
 		} else {
 			exit('No access to this page');
 		}
