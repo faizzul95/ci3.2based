ff84306c2e9e116293576fa31ac91fbe71dfee7a
Fahmy Izwan <mfahmyizwan@gmail.com>
2023-07-10 11:06:56 +0800

    Update common.blade.php

--- application/views/_generals/php/common.blade.php
+++ application/views/_generals/php/common.blade.php
@@ -78,4 +78,83 @@ if (isset($_POST['fileName'])) {
 			}
 		});
 	}
+
+	function loadFormContent(fileName, idToLoad, sizeModal = 'lg', urlFunc = null, title = 'Default Title', dataArray =
+		null, typeModal = 'modal') {
+
+		if (typeModal == 'modal') {
+			var idContent = idToLoad + "-" + sizeModal;
+		} else {
+			var idContent = "offCanvasContent-right";
+		}
+
+		$('#' + idContent).empty(); // reset
+
+		return $.ajax({
+			type: "POST",
+			url: $('meta[name="base_url"]').attr('content') + 'public/custom/php/general.php',
+			data: {
+				baseUrl: $('meta[name="base_url"]').attr('content'),
+				fileName: fileName,
+				dataArray: dataArray,
+				// _token: Cookies.get(csrf_cookie_name)
+				'cid': Cookies.get(csrf_cookie_name) // csrf token
+			},
+			headers: {
+				"Authorization": "Bearer " + Cookies.get(csrf_cookie_name),
+				"X-CSRF-TOKEN": Cookies.get(csrf_cookie_name),
+			},
+			dataType: "html",
+			success: function(response) {
+				$('#' + idContent).append(response);
+
+				setTimeout(function() {
+					if (typeof getPassData == 'function') {
+						getPassData($('meta[name="base_url"]').attr('content'), Cookies.get(
+							csrf_cookie_name), dataArray);
+					} else {
+						console.log('function getPassData not initialize!');
+					}
+				}, 50);
+
+				// get form id
+				var formID = $('#' + idContent + ' > form').attr('id');
+				// > div:first-child
+
+				$("#" + formID)[0].reset(); // reset form
+				document.getElementById(formID).reset(); // reset form
+				$("#" + formID).attr('action', urlFunc); // set url
+
+				if (typeModal == 'modal') {
+					$('#generalTitle-' + sizeModal).text(title);
+					$('#generalModal-' + sizeModal).modal('show');
+					$("#" + formID).attr("data-modal", '#generalModal-' + sizeModal);
+				} else {
+					// reset
+					$('.custom-width').css('width', '400px');
+
+					$('#offCanvasTitle-right').text(title);
+					$('#generaloffcanvas-right').offcanvas('toggle');
+					$("#" + formID).attr("data-modal", '#generaloffcanvas-right');
+					$('.custom-width').css('width', sizeModal);
+				}
+
+				if (dataArray != null) {
+					$.each($('input, select ,textarea', "#" + formID), function(k) {
+						var type = $(this).prop('type');
+						var name = $(this).attr('name');
+
+						if (type == 'radio' || type == 'checkbox') {
+							$("input[name=" + name + "][value='" + dataArray[name] + "']").prop(
+								"checked", true);
+						} else {
+							$('#' + name).val(dataArray[name]);
+						}
+
+					});
+				}
+
+			}
+		});
+	}
 </script>
