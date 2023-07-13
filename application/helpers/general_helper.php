fa689046d0e801369c8cb6087a135d52a3b593ad
Fahmy Izwan <mfahmyizwan@gmail.com>
2023-07-14 01:01:47 +0800

    Update general_helper.php

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -72,9 +72,9 @@ if (!function_exists('reduceDate')) {
 // CURRENCY & MONEY HELPERS SECTION
 
 if (!function_exists('currency_format')) {
-	function currency_format($amount)
+	function currency_format($amount, $decimal = 2)
 	{
-		return number_format((float)$amount, 2, '.', ',');
+		return number_format((float)$amount, $decimal, '.', ',');
 	}
 }
 
@@ -225,46 +225,46 @@ if (!function_exists('isError')) {
 }
 
 if (!function_exists('hasData')) {
-    function hasData($data = NULL, $arrKey = NULL, $returnData = false, $defaultValue = NULL)
-    {
-        $response = false; // default return
+	function hasData($data = NULL, $arrKey = NULL, $returnData = false, $defaultValue = NULL)
+	{
+		$response = false; // default return
 
-        // check if data is exist
-        if (isset($data)) {
-            if (($data !== '' || $data !== NULL || $data !== 'null') && !empty($data)) {
-                // check if arrKey is exist and not null
-                if (!empty($arrKey)) {
-                    $keys = explode('.', $arrKey);
-                    $keyArr = $keys[0];
+		// check if data is exist
+		if (isset($data)) {
+			if (($data !== '' || $data !== NULL || $data !== 'null') && !empty($data)) {
+				// check if arrKey is exist and not null
+				if (!empty($arrKey)) {
+					$keys = explode('.', $arrKey);
+					$keyArr = $keys[0];
 
-                    if (count($keys) <= 1) {
-                        if (is_array($data) && array_key_exists($keyArr, $data)) {
-                            $response = !empty($data[$keyArr]) ? true : false;
-                        } else if (is_object($data) && isset($data->$keyArr)) {
-                            $response = !empty($data->$keyArr) ? true : false;
-                        }
-                    } else {
-                        $remainingKeys = implode('.', array_slice($keys, 1));
-                        if (is_array($data) && array_key_exists($keyArr, $data)) {
-                            $response = hasData($data[$keyArr], $remainingKeys, $returnData, $defaultValue);
-                        } else if (is_object($data) && isset($data->$keyArr)) {
-                            $response = hasData($data->$keyArr, $remainingKeys, $returnData, $defaultValue);
-                        }
-                    }
-                } else if (empty($arrKey)) {
-                    $response = true;
-                } else {
-                    $response = false;
-                }
-            }
-        }
+					if (count($keys) <= 1) {
+						if (is_array($data) && array_key_exists($keyArr, $data)) {
+							$response = !empty($data[$keyArr]) ? true : false;
+						} else if (is_object($data) && isset($data->$keyArr)) {
+							$response = !empty($data->$keyArr) ? true : false;
+						}
+					} else {
+						$remainingKeys = implode('.', array_slice($keys, 1));
+						if (is_array($data) && array_key_exists($keyArr, $data)) {
+							$response = hasData($data[$keyArr], $remainingKeys, $returnData, $defaultValue);
+						} else if (is_object($data) && isset($data->$keyArr)) {
+							$response = hasData($data->$keyArr, $remainingKeys, $returnData, $defaultValue);
+						}
+					}
+				} else if (empty($arrKey)) {
+					$response = true;
+				} else {
+					$response = false;
+				}
+			}
+		}
 
-        // if return data is set to true it will return the data instead of bool,
-        if ($returnData)
-            return $response && !empty($arrKey) ? $data[$arrKey] : ($response ? $data : $defaultValue);
-        else
-            return $response;
-    }
+		// if return data is set to true it will return the data instead of bool,
+		if ($returnData)
+			return $response && !empty($arrKey) ? $data[$arrKey] : ($response ? $data : $defaultValue);
+		else
+			return $response;
+	}
 }
 
 if (!function_exists('fileExist')) {
