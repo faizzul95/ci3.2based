7c7ab8855dd3713f76c0bcd317f39d7579980319
Fahmy Izwan <mfahmyizwan@gmail.com>
2023-08-21 13:53:43 +0800

    Update general_helper.php

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -246,9 +246,7 @@ if (!function_exists('isSuccess')) {
 
 			$code = (is_string($response)) ? (int)$response : $response;
 
-			if (in_array($code, $successStatus)) {
-				return true;
-			}
+			return in_array($code, $successStatus);
 		}
 
 		return false;
@@ -267,55 +265,56 @@ if (!function_exists('isError')) {
 
 			$code = (is_string($response)) ? (int)$response : $response;
 
-			if (in_array($code, $errorStatus)) {
-				return true;
-			}
+			return in_array($code, $errorStatus);
 		}
 
 		return false;
 	}
 }
 
+/**
+ * Check if the provided data contains non-empty values for the specified key.
+ *
+ * @param mixed       $data          The data to be checked (array or string).
+ * @param string|null $arrKey        The key to check within the data.
+ * @param bool        $returnData    If true, returns the data value if found.
+ * @param mixed       $defaultValue  The default value to return if data is not found.
+ *
+ * @return bool|string|null Returns true if data exists, data value if $returnData is true and data exists, otherwise null or $defaultValue.
+ */
 if (!function_exists('hasData')) {
 	function hasData($data = NULL, $arrKey = NULL, $returnData = false, $defaultValue = NULL)
 	{
-		$response = false; // default return
-
-		// check if data is exist
-		if (isset($data)) {
-			if (($data !== '' || $data !== NULL || $data !== 'null') && !empty($data)) {
-				// check if arrKey is exist and not null
-				if (!empty($arrKey)) {
-					$keys = explode('.', $arrKey);
-					$keyArr = $keys[0];
-
-					if (count($keys) == 1) {
-						if (is_array($data) && array_key_exists($keyArr, $data)) {
-							$response = !empty($data[$keyArr]) ? true : false;
-						} else if (is_object($data) && isset($data->$keyArr)) {
-							$response = !empty($data->$keyArr) ? true : false;
-						}
-					} else {
-						$remainingKeys = implode('.', array_slice($keys, 1));
-						if (is_array($data) && array_key_exists($keyArr, $data)) {
-							$response = hasData($data[$keyArr], $remainingKeys, $returnData, $defaultValue);
-						} else if (is_object($data) && isset($data->$keyArr)) {
-							$response = hasData($data->$keyArr, $remainingKeys, $returnData, $defaultValue);
-						}
-					}
-				} else if (empty($arrKey)) {
-					$response = true;
-				} else {
-					$response = false;
-				}
-			}
+		// Check if data is not set, empty, or null
+		if (!isset($data) || empty($data) || is_null($data)) {
+			return false;
 		}
 
-		// if return data is set to true it will return the data instead of bool,
-		if ($returnData)
-			return $response && !empty($arrKey) ? stringToNestedArray($arrKey, $data, $defaultValue) : ($response ? $data : $defaultValue);
-		else
-			return $response;
+		// If arrKey is not provided, consider data itself as having data
+		if (is_null($arrKey)) {
+			return true;
+		}
+
+		// Split the keys into an array
+		$keys = explode('.', $arrKey);
+		$currentKey = array_shift($keys);
+
+		// Check if data is an array and the current key exists
+		if (is_array($data) && array_key_exists($currentKey, $data)) {
+			// If no more keys left, return the data or true based on returnData flag
+			if (empty($keys)) {
+				if ($returnData) {
+					return $data[$currentKey] ?? $defaultValue;
+				}
+				return true;
+			}
+
+			// Recursively call hasData for the nested key
+			return hasData($data[$currentKey], implode('.', $keys), $returnData, $defaultValue);
+		}
+
+		// No match found, return false or return default value
+		return $returnData ? $defaultValue : false;
 	}
 }
 
